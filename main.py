fb4e0077a406d86b86b394d9c2c4378ad3c4153a
Hien Tran <htran174@csu.fullerton.edu>
2025-02-27 16:22:23 -0800

    added comment for readablity

--- main.py
+++ main.py
@@ -64,13 +64,13 @@ def generate_sorting():
     user_input = entry.get()
     search_value = search_entry.get()
     
-    if input_mode.get() == "Random":
-        if not user_input.isdigit() or int(user_input) <= 0:
+    if input_mode.get() == "Random": #if user select Random
+        if not user_input.isdigit() or int(user_input) < 0: #value needs to be digit and > 0
             messagebox.showerror("Input Error", "Please enter a valid positive integer for the number of elements.")
             return
         num_elements = int(user_input)
         random_array = [random.randint(1, 999) for _ in range(num_elements)]
-    else:
+    else: #if user input own array
         try:
             random_array = list(map(int, user_input.split(',')))
         except ValueError:
@@ -80,41 +80,40 @@ def generate_sorting():
     if (not search_value.isdigit()) and sort_var[4].get(): #show error box if search is not inputed
         messagebox.showerror("Input Error", "Please enter a valid positive integer for Linear Search.")
         return
-    if search_value.isdigit() and not sort_var[4].get():
+    if search_value.isdigit() and not sort_var[4].get(): #show error box if user inputed search value without selcting Linear Search
         messagebox.showerror("Input Error", "Please check Linear Search if you want to use it")
         return
     
-
-    print(f"Generated Array: {random_array}")
-    sort_time_dictionary = {}
+    sort_time_dictionary = {} #creating a dictionary 
     
     messagebox.showinfo("Selection Confirmed", f"Array: {random_array}")
     
     if sort_var[0].get():
         time = bubble_sort(random_array.copy())
-        sort_time_dictionary['Bubble Sort'] = time * 1000000
+        sort_time_dictionary['Bubble Sort'] = time * 1000000 #adding to dictionary and converting time to microseconds
 
     if sort_var[1].get():
         time = merge_sort(random_array.copy())
-        sort_time_dictionary['Merge Sort'] = time * 1000000
+        sort_time_dictionary['Merge Sort'] = time * 1000000 #adding to dictionary and converting time to microseconds
 
     if sort_var[2].get():
         time = quick_sort(random_array.copy())
-        sort_time_dictionary['Quick Sort'] = time * 1000000
+        sort_time_dictionary['Quick Sort'] = time * 1000000 #adding to dictionary and converting time to microseconds
 
     if sort_var[3].get():
         time = radix_sort(random_array.copy(), 0)
-        sort_time_dictionary['LSD Radix Sort'] = time * 1000000
+        sort_time_dictionary['LSD Radix Sort'] = time * 1000000 #adding to dictionary and converting time to microseconds
         time = radix_sort(random_array.copy(), 1)
-        sort_time_dictionary['MSD Radix Sort'] = time * 1000000
+        sort_time_dictionary['MSD Radix Sort'] = time * 1000000 #adding to dictionary and converting time to microseconds
 
     if sort_var[4].get():
         time = linear_search(int(search_value), random_array.copy())
-        sort_time_dictionary['Linear Search'] = time * 1000000
+        sort_time_dictionary['Linear Search'] = time * 1000000 #adding to dictionary and converting time to microseconds
 
     graph.show_graph(sort_time_dictionary)
 
 if __name__ == "__main__":
+    #Creating main GUI for user input
     root = tk.Tk()
     root.title("Visualization of Sorting Algorithms")
     root.geometry("400x400")
@@ -140,6 +139,6 @@ if __name__ == "__main__":
     search_entry = tk.Entry(linear_search_frame)
     search_entry.pack(side='left', padx=5)
     
-    tk.Button(root, text="Generate", command=generate_sorting).pack(pady=10)
+    tk.Button(root, text="Generate", command=generate_sorting).pack(pady=10) 
     
     root.mainloop()
